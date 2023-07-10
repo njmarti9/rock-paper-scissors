492cc71d368fc2f2abd66a4faba3ed18fe908142
njmarti9 <njmarti9@asu.edu>
2023-07-10 12:30:27 -0700

    Gave the game a UI and changed some logic: - Added buttons to choose selection - Added score tracking on the UI - Every game is now first to 5 wins

--- game.js
+++ game.js
@@ -1,71 +1,100 @@
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"];
+var player = 0, computer = 0;
+
+const container = document.querySelector('#container');
+
+const content = document.createElement('h1');
+content.classList.add('title');
+content.textContent = 'Rock Paper Scissors!!!';
+container.appendChild(content);
+
+const rock = document.querySelector('#rockBtn');
+rock.addEventListener('click', function (e) {
+    console.log(playRound("rock", getComputerChoice()));
+    checkForWin();
+});
+
+const paper = document.querySelector('#paperBtn');
+paper.addEventListener('click', function (e) {
+    console.log(playRound("paper", getComputerChoice()));
+    checkForWin();
+});
+
+const scissors = document.querySelector('#scissorsBtn');
+scissors.addEventListener('click', function (e) {
+    console.log(playRound("scissors", getComputerChoice()));
+    checkForWin();
+});
+
+/*const buttons = document.querySelectorAll('#btns');
+for (i in buttons){
+    buttons[i].addEventListener('click', function (e){
+        console.log(playRound(buttons[i].getAttributeNames, getComputerChoice()));
+        checkForWin();
+    });
+}*/
+
+const playerScore = document.createElement('h3');
+playerScore.classList.add('playerScore');
+playerScore.textContent = 'Player: ' + player;
+container.appendChild(playerScore);
+
+const computerScore = document.createElement('h3');
+computerScore.classList.add('computerScore');
+computerScore.textContent = 'Computer: ' + computer;
+container.appendChild(computerScore);
+
+const results = document.createElement('p');
+results.classList.add('results');
+results.textContent = 'Press a Button Below:';
+container.append(results);
+
+function checkForWin(){
+    if (player == 5)
+    {
+        results.textContent = 'You have made it to 5 wins and have won the game!';
+        player = 0;
+        computer = 0;
+        playerScore.textContent = 'Player: ' + player;
+        computerScore.textContent = 'Computer: ' + computer;
+    }
+    else if (computer == 5)
+    {
+        results.textContent = 'The computer has made it to 5 wins and has won the game :(';
+        player = 0;
+        computer = 0;
+        playerScore.textContent = 'Player: ' + player;
+        computerScore.textContent = 'Computer: ' + computer;
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
+    console.log("Player: " + playerSelection + " Computer: " + computerSelection);
     if ((playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper"))
     {
+        results.textContent = "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        player++;
+        playerScore.textContent = 'Player: ' + player;
         return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
     }
     else if ((computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper"))
     {
+        results.textContent = "You Lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        computer++;
+        computerScore.textContent = 'Computer: ' + computer;
         return "You Lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
     }
     else
     {
+        results.textContent = "It's a tie!";
         return "It's a tie!"
     }
   }
 
 function getComputerChoice(){
     return choices[Math.floor(Math.random()*3)];
-}
-
-function getPlayerChoice(){
-    var choice = window.prompt("Choose Rock, Paper, or Scissors", "rock");
-    if (choice.toLowerCase() == "rock" || 
-    choice.toLowerCase() == "paper" ||
-    choice.toLowerCase() == "scissors")
-    {
-        return choice.toLowerCase()
-    }
-    else {
-        return "ERROR"
-    }
-}
-
-function game(){
-    let wins = 0, losses = 0;
-    for (let i = 0; i < 5; i++){
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-
-        console.log("Player Choice: " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1))
-        console.log("Computer Choice: " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1))
-        let round = playRound(playerSelection, computerSelection)
-        let decision = round.split(" ");
-        if (decision[1] === "Win!")
-        {
-            wins++;
-        }
-        else if (decision[1] === "Lose!")
-        {
-            losses++;
-        }
-        console.log("Play round: " + round);
-    }
-    if (wins > losses)
-    {
-        console.log("--------You Win The Game!--------")
-    }
-    else if (losses > wins)
-    {
-        console.log("--------You Lose The Game!--------")
-    }
-    else
-    {
-        console.log("--------It's A Tie Game!--------")
-    }
 }
